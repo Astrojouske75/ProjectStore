44c811b1dffe32ca19e1a270178b14218806a383
FLIA <acerocamilo7@gmail.com>
2023-02-09 09:01:09 -0500

    inicio de proyecto con springboot

---
+++ src/main/java/com/ProjectStore/ProjectStore/Controller/TestController.java
@@ -0,0 +1,16 @@
+package com.ProjectStore.ProjectStore.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @RequestMapping
+    @ResponseBody
+    public ResponseEntity<String>function(){
+        return new ResponseEntity<>("hola desde el controller", HttpStatus.OK);
+    }
+}
